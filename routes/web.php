c99f425c6964dc23af6abc9d76ab27c84c989d5b
hid0 <faizhid11@gmail.com>
2024-09-22 17:33:48 +0700

    implement some admin dashboard in inertia

--- routes/web.php
+++ routes/web.php
@@ -24,4 +24,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+Route::get('/try', function () {
+    return Inertia::render('Dashboard');
+});
+
+require __DIR__ . '/auth.php';
