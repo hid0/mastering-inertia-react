54f1d473e5c0575b9a9822f8f32a17dc9ebc3e29
hid0 <faizhid11@gmail.com>
2025-05-07 15:23:54 +0700

    start learning again

---
+++ resources/views/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title></title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Instrument+Sans:ital,wght@0,400..700;1,400..700&display=swap"
+      rel="stylesheet">
+    @vite('resources/js/app.js')
+    @inertiaHead
+  </head>
+
+  <body>
+    @inertia()
+  </body>
+
+</html>
