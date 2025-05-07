8074c8b1fbceb77dd5823c3037ffe8cbc9456d22
hid0 <faizhid11@gmail.com>
2025-05-07 16:03:39 +0700

    next step

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,16 +5,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title></title>
+    <title>{{ $title ?? '' }} &mdash; {{ config('app.name', 'Laravel') }}</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Instrument+Sans:ital,wght@0,400..700;1,400..700&display=swap"
       rel="stylesheet">
-    @vite('resources/js/app.js')
+    @viteReactRefresh
+    @vite(['resources/css/app.css', 'resources/js/app.jsx'])
     @inertiaHead
   </head>
 
-  <body>
+  <body class="antialiased">
     @inertia()
   </body>
 
